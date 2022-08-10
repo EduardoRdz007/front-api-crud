57720da9bcb64bc4128f69bd0226a87540971b24
EduardoRdz007 <edurdz010@gmail.com>
2022-08-10 00:03:17 -0500

    feat: Added the botto like for each post

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,8 +1,17 @@
 import "./post.css";
 import { Link } from "react-router-dom";
-
+import FavoriteContext from "../../context/global/likecontext.context";
+import { useContext } from "react";
 export default function Post({ post }) {
   const PF = "http://localhost:5000/images/";
+  const {favoritePokemons, updateFavorite} = useContext(FavoriteContext);
+  const redHeart = "👍";
+    const blackHeart = "👨";
+    const heart = favoritePokemons.includes() ? blackHeart : redHeart ;
+  const clickHeart = (e) => {
+    e.preventDefault();
+      updateFavorite();
+}
   return (
     <div className="post">
       {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
@@ -16,6 +25,9 @@ export default function Post({ post }) {
           <span className="postTitle">{post.title}</span>
         </Link>
         <hr />
+        <button onClick={clickHeart}>
+                        <div className="favorite">{heart}</div>
+                    </button>
         <span className="postDate">
           {new Date(post.createdAt).toDateString()}
         </span>
