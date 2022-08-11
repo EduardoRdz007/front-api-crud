46f08266a557ae564e4c9d302c11e4cebdd08f2b
AlonsoAlfa <afaustoalonso@gmail.com>
2022-08-11 11:02:31 -0500

    feat:Correction of labels for the creation of the logo of the web page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,8 +4,8 @@ export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">React & Node</span>
-        <span className="headerTitleLg">Blog</span>
+      <span className="headerTitleSm">Comic</span>
+        <span className="headerTitleSm2">Book</span>
       </div>
       <img
         className="headerImg"
