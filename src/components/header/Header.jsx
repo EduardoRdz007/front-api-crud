10aaffed89efbbff5bcb1cc322b7c91552f7d811
AlonsoAlfa <afaustoalonso@gmail.com>
2022-08-11 11:09:36 -0500

    refactor: Add new background in the home

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
       </div>
       <img
         className="headerImg"
-        src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        src="https://free4kwallpapers.com/uploads/originals/2016/05/28/avengers-wallpaper.jpg"
         alt=""
       />
     </div>
